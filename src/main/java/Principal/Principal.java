b55890b82e0d4cbfaa95369adfe4d9dfa9153ac6
1072415373 <1072415373@UNS11DT098793.animaedu.intranet>
2024-04-02 11:16:57 -0300

    Inicio Projeto

---
+++ src/main/java/Principal/Principal.java
@@ -0,0 +1,19 @@
+package Principal;
+
+import javax.swing.JOptionPane;
+import model.Triangulo;
+
+public class Principal {
+    public static void main(String[] args){
+        
+        Triangulo tril = new Triangulo();
+        
+        tril.setBase(Double.parseDouble(JOptionPane.
+                showInputDialog("Digite a base do triangulo")));
+        
+        tril.setAltura(Double.parseDouble(JOptionPane.
+                showInputDialog("Digite a altura do triangulo")));
+        
+        JOptionPane.showMessageDialog(null,"A área do triangulo é " + tril.getArea());
+    }
+}
